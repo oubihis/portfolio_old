31fc46fdb52dfda40e9cb4f376de251d256403ef
oubihis <oubihis@gmail.com>
2019-09-16 15:31:49 +0100

    Add Technical Skill Set & Logos

--- src/components/featuresPage.vue
+++ src/components/featuresPage.vue
@@ -1,44 +1,108 @@
 <template>
 
-
-    <v-layout row wrap align-start justify-center>
-
-
-        <v-flex v-for="feature in features" :key="feature.title"  v-bind="{ [`lg${feature.lg} md${feature.md} sm${feature.sm} xs${feature.xs} mr${feature.mr}`]: true }"  >
-        <v-feature class="mx-1 my-2">
-            <v-img :src="feature.src" height="40"></v-img>
-            <v-feature-title primary-title>
-            <div>
-                <div class="headline" v-text="feature.title"></div>
-                <span class="grey--text" v-text="feature.subtitle"></span>
-            </div>
-            </v-feature-title>
-            <v-feature-actions>
-            <a :href="feature.url" :target="feature.target">
-            <v-btn flat color="blue-grey darken-4">See Github Repo</v-btn>
-            </a>
-            </v-feature-actions>
-        </v-feature>
-        </v-flex>
+  <div grid-list-md text-xs-center>
+<h1 class="blue-grey--text darken-4 font-weight-bold my-4 text-xs-left" >
+    {{titleOfThelogos}}
+  </h1>
+    <v-layout row wrap>
+      <v-flex v-for="feature in features" :key="feature.title" v-bind="{ [`lg${feature.lg} md${feature.md} sm${feature.sm} xs${feature.xs} mr${feature.mr}`]: true }">
+          <v-img :src="feature.src" aspect-ratio="2" contain class="mx-1 my-3"></v-img>
+      </v-flex>
     </v-layout>
-
+  </div>
 </template>
+
 
 <script>
  export default {
     data: () => ({
+        titleOfThelogos: 'Technical Skill Set',
         features: [
             {
-              lg: 3,
-              md: 6,
+              lg: 2,
+              md: 4,
               sm: 6,
-              xs: 12,
-              src: require('../assets/features/nodejs.png'),
-              title: 'Weather Widget',
-              subtitle: 'Vanilla Javascript - Darksky API - Skycons animated weather glyphs',
+              xs: 6,
+              src: require('../assets/features/html5.png'),
+            },
+            {
+              lg: 2,
+              md: 4,
+              sm: 6,
+              xs: 6,
+              src: require('../assets/features/css3.png'),
+            },
+            {
+              lg: 2,
+              md: 4,
+              sm: 6,
+              xs: 6,
+              src: require('../assets/features/sass.png'),
+            },
+            {
+              lg: 2,
+              md: 4,
+              sm: 6,
+              xs: 6,
+              src: require('../assets/features/bootstrap.png'),
+            },
+            {
+              lg: 2,
+              md: 4,
+              sm: 6,
+              xs: 6,
+              src: require('../assets/features/javascript.png'),
+            },
+            {
+              lg: 2,
+              md: 4,
+              sm: 6,
+              xs: 6,
+              src: require('../assets/features/jquery.png'),
+            },
+            {
+              lg: 2,
+              md: 4,
+              sm: 6,
+              xs: 6,
+              src: require('../assets/features/vuejs.png'),
+            },
+            {
+              lg: 2,
+              md: 4,
+              sm: 6,
+              xs: 6,
+              src: require('../assets/features/photoshop.png'),
+            },
+            {
+              lg: 2,
+              md: 4,
+              sm: 6,
+              xs: 6,
+              src: require('../assets/features/gulp.png'),
+            },
+            {
+              lg: 2,
+              md: 4,
+              sm: 6,
+              xs: 6,
+              src: require('../assets/features/mysql.png'),
+            },
+            {
+              lg: 2,
+              md: 4,
+              sm: 6,
+              xs: 6,
+              src: require('../assets/features/scrapy.png'),
+            },
+            {
+              lg: 2,
+              md: 4,
+              sm: 6,
+              xs: 6,
+              src: require('../assets/features/ux-ui.svg'),
             }
         ]
     })
   }
 </script>
-
